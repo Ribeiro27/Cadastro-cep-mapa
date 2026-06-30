077539146f98b94561100bbbf4d869f55e41cd51
Jessé Ribeiro <Jesse.ribeiro27@gmail.com>
2026-06-30 17:46:38 -0300

    iniciando o projeto

---
+++ js/main.js
@@ -0,0 +1,3 @@
+import navbar from "./navbar/navbar.js";
+
+const app = document.getElementById('app');
---
+++ js/navbar/navbar.js
@@ -0,0 +1,6 @@
+const navbar = document.getElementById('navbar');
+console.log()
+navbar.innerHTML = `<nav class="bem-navbar">
+<h1> Cadastro de cep + Mapa</h1>
+
+</nav>`
